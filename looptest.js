e2b4bdc3f1671f06c38aac2274873f4e35e499d9
Taiyab Husain <120089181+Husain1212@users.noreply.github.com>
2023-04-04 11:58:46 +0530

    Update looptest.js
    change formate

--- looptest.js
+++ looptest.js
@@ -1,108 +1,99 @@
-Ans-1-------------
+## Ans-1 print 1 to 100 number
+```
 for(i = 1; i<=100; i++){
     console.log(i);
 }
+```
 
-Ans--2---------------odd number
-
+## Ans--2 print odd number
+```
 for(i = 1; i<=100; i++){
     if(i % 2===1){
         console.log(i);
-    }
-    
+    } 
 }
+```
 
-Ans--3---------------even number
-
+## Ans--3 print  even number
+```
 for(i = 1; i<=100; i++){
     if(i % 2===0){
         console.log(i);
-    }
-    
+    }    
 }
-
-Ans--4--------------------5 multiple
-
+```
+## Ans--4 print numbers 5 multiple
+```
 for(i =1; i<=100; i++){
     if(i % 5 === 0){
         console.log(i);
     }
 }
-
-
-Ans--5--------------------5 && 15 multiple
-
+```
+## Ans--5 print number 5 && 15 multiple
+```
 for(i =1; i<=100; i++){
     if(i % 5 === 0 && i % 15 === 0){
         console.log(i);
     }
 }
-
-Ans--6--------------------2s table
-
+```
+## Ans--6 print 2 table of numbers
+```
 for(i =1; i<=20; i++){
     if(i % 2 === 0){
         console.log(i);
     }
 }
-
-Ans--7--------------------print according to user
-
-
+```
+## Ans--7 print number according to user
+```
 let user = Number(prompt("please enter number"))
 
 for(i =1; i<=user; i++){
         console.log(i , "Helloworld");
     }
-
-
-Ans--8--------------------add sum number
-
+```
+## Ans--8 print 1 to 100 all sum of number
+```
 let sum= 1;
 for(i =1; i<=100; i++){
     sum = sum + i;}
         console.log("The sum of all numbers:", sum);
-    
-
-
-
-Ans--9--------------------add sum number odd
+```    
+## Ans--9  print between 1 to 100 sum odd numbers
+```
 let sum= 0;
 for(i =1; i<=100; i++){
     if(i % 2===1){
         sum = sum + i;}}
 
-        console.log("The sum of all odd numbers:", sum);
-    
-    
-    
+        console.log("The sum of all odd numbers:", sum); 
 }
-
-Ans--10--------------------add sum number even
+```
+## Ans--10print between 1 to 100 sum even numbers
+```
 let sum= 0;
 for(i =1; i<=100; i++){
     if(i % 2===0){
         sum = sum + i;}}
         console.log("The sum of all even numbers:", sum);
-
-
-Ans--11---------------- print 1,2,4,8,16,32,64.......upto 2048
-
-
+```
+## Ans--11print series 1,2,4,8,16,32,64.......upto 2048
+```
 for(i =1; i<=2048; i=i*2){
         console.log(i);
     }
-    
-Ans-----------print a,c,e,g.......upto last latter
-
+``` 
+## Ans-11print series of a,c,e,g.......upto last latter
+```
 for (i=97; i<=122; i++){
     console.log(String.fromCharCode(i))
 }
-
-
-Ans-12----------------------------------------
-
-
+```
+## Ans-12 print 1 to hundred number when number start 11,21,31,----91
+in new line?
+```
 for (let f = 1; f <= 100; f++) {
   if (
     f === 11 ||
@@ -119,19 +110,18 @@ for (let f = 1; f <= 100; f++) {
   }
   document.write(f, ",");
 }
+```
 
-logic  ===============================
-
+```
 for(let f = 1; f <= 100; f++){
     document.write(f,",") ;
     if(f % 10 === 0){
         document.write("<br>")
     }
 }
-
-Ans-13----------------------------------------
-
-
+```
+## Ans-13 print fizzbuzz when number is divisble 3 && 5 in 1 to 100 ?
+```
 for(i =1; i<=100; i++){
     if(i % 3 ===0 && i % 5 === 0){
     console.log("fizzbuzz");
@@ -139,16 +129,25 @@ for(i =1; i<=100; i++){
         console.log(i);
     }
     }
-
-Ans-14----------------------------------------
-
+```
+## Ans-14 print 2 table according to this ..
+```
+2*1=2
+2*2=4
+2*3=6
+.
+.
+2*10=20
+```
+```
 for(i=1;  i<=10; i++){
 
         console.log("2" + "*" + i + "=" + 2*i);
     }
+```
 
-Ans 15-----------------------------------
-
+## Ans 15 print A B C D E in 10 series
+```
 for(i = 1; i<=50; i++){
     if(i<=10){
         console.log("A");
@@ -164,7 +163,7 @@ for(i = 1; i<=50; i++){
     }
     
 }
-
+```
 Ans 16-------------------------------
 
 for(i = 50; i>=1; i--){
